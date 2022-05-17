8fcc010799b0163ca4842d0c167bd0dd2443edd8
Bradley <bradley.t.sheehan@gmail.com>
2022-05-16 19:25:24 -0600

    rel=noreferrer

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -10,22 +10,22 @@ function Contact() {
                 <div className={styles["abstract"]}>
 
                     <h1>
-                    Email: <a href="mailto:bradley.t.sheehan@gmail.com" target="_blank" rel="noopener">bradley.t.sheehan@gmail.com</a>
+                    Email: <a href="mailto:bradley.t.sheehan@gmail.com" target="_blank" rel="noreferrer">bradley.t.sheehan@gmail.com</a>
                     </h1>
                     <h1>
-                        GitHub: <a href="https://github.com/RedVelvetZip" target="_blank" rel="noopener">github.com/RedVelvetZip</a>
+                        GitHub: <a href="https://github.com/RedVelvetZip" target="_blank" rel="noreferrer">github.com/RedVelvetZip</a>
                     </h1>
                     <h1>
-                        Twitter: <a href="https://twitter.com/RedVelvetZip" target="_blank" rel="noopener">twitter.com/RedVelvetZip</a>
+                        Twitter: <a href="https://twitter.com/RedVelvetZip" target="_blank" rel="noreferrer">twitter.com/RedVelvetZip</a>
                     </h1>
                     <h1>
-                        Discord: <a href="https://discordapp.com/users/redvelvet#4018" target="_blank" rel="noopener">redvelvet#4018</a>
+                        Discord: <a href="https://discordapp.com/users/redvelvet#4018" target="_blank" rel="noreferrer">redvelvet#4018</a>
                     </h1>
                     <h1>
-                        Upwork: <a href="https://www.upwork.com/freelancers/~013dc5250828035a2c" target="_blank" rel="noopener">upwork.com/freelancers/~013dc5250828035a2c</a>
+                        Upwork: <a href="https://www.upwork.com/freelancers/~013dc5250828035a2c" target="_blank" rel="noreferrer">upwork.com/freelancers/~013dc5250828035a2c</a>
                     </h1>
                     <h1>
-                        LinkedIn: <a href="https://www.linkedin.com/in/bradleysheehan/" target="_blank" rel="noopener">linkedin.com/in/BradleySheehan</a>
+                        LinkedIn: <a href="https://www.linkedin.com/in/bradleysheehan/" target="_blank" rel="noreferrer">linkedin.com/in/BradleySheehan</a>
                     </h1>
                 </div>
 
