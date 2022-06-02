0240df26a9531d6b4cf95ccb86179b35548d8fb3
Bradley <bradley.t.sheehan@gmail.com>
2022-06-02 15:53:40 -0600

    remove author from posts

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -73,7 +73,7 @@ const PostDetail = ({ post }) => {
         </div>
         <div className="px-4 lg:px-0">
           <div className="flex items-center mb-8 w-full">
-            <div className="hidden md:flex items-center justify-center lg:mb-0 lg:w-auto mr-8 items-center">
+            {/* <div className="hidden md:flex items-center justify-center lg:mb-0 lg:w-auto mr-8 items-center">
               <img
                 alt={post.author.name}
                 height="30px"
@@ -82,7 +82,7 @@ const PostDetail = ({ post }) => {
                 src={post.author.photo.url}
               />
               <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
-            </div>
+            </div> */}
             <div className="font-medium text-gray-700">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
