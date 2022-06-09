3a5087bf3fb9042812d0a3c007f826f71add4979
Bradley <bradley.t.sheehan@gmail.com>
2022-06-09 10:48:06 -0600

    search replace w blog

--- pages/search.js
+++ pages/search.js
@@ -1,22 +1,57 @@
-import { PostCardSmall } from "../components";
-import { getPosts } from "../services";
-import BlogLayout from "../components/Layout";
-import styles from "../styles/search.module.scss";
+// import { PostCardSmall } from "../components";
+// import { getPosts } from "../services";
+// import BlogLayout from "../components/Layout";
+// import styles from "../styles/search.module.scss";
 
-export default function Search({ posts }) {
+// export default function Search({ posts }) {
+//   return (
+//     <main className={styles["search"]}>
+//       <div className={styles["flex-container"]}>
+//         {posts.map((post, index) => (
+//           <div className={styles["flex-item"]}>
+//             <PostCardSmall key={index} post={post.node} />
+//           </div>
+//         ))}
+//       </div>
+//     </main>
+//   );
+// }
+// Search.layout = BlogLayout;
+
+// // Fetch data at build time
+// export async function getStaticProps() {
+//   const posts = (await getPosts()) || [];
+//   return {
+//     props: { posts },
+//   };
+// }
+
+import { FeaturedPosts } from '../sections/index';
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
+import BlogLayout from '../components/Layout'
+
+export default function Blog({ posts }) {
   return (
-    <main className={styles["search"]}>
-      <div className={styles["flex-container"]}>
-        {posts.map((post, index) => (
-          <div className={styles["flex-item"]}>
-            <PostCardSmall key={index} post={post.node} />
+    <div className="container mx-auto px-10 mb-8">
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
           </div>
-        ))}
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
-Search.layout = BlogLayout;
+Blog.layout = BlogLayout;
 
 // Fetch data at build time
 export async function getStaticProps() {
@@ -25,3 +60,5 @@ export async function getStaticProps() {
     props: { posts },
   };
 }
+
+
