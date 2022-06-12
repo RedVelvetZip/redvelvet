6116cb12411c46d335cab42c5086de014a6cc69d
Bradley <bradley.t.sheehan@gmail.com>
2022-06-12 10:00:05 -0600

    search WIP

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,8 @@
 // import { PostCardSmall } from "../components";
 // import { getPosts } from "../services";
 // import BlogLayout from "../components/Layout";
-// import styles from "../styles/search.module.scss";
+import styles from "../styles/search.module.scss";
+import React, { useState, useEffect } from 'react';
 
 // export default function Search({ posts }) {
 //   return (
@@ -26,32 +27,40 @@
 //   };
 // }
 
-import { FeaturedPosts } from '../sections/index';
-import { PostCard, Categories, PostWidget } from '../components';
-import { getPosts } from '../services';
-import BlogLayout from '../components/Layout'
+import { PostCardSmall, PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
+import BlogLayout from "../components/Layout";
 
-export default function Blog({ posts }) {
+export default function Search({ posts }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <FeaturedPosts />
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
-            <PostCard key={index} post={post.node} />
-          ))}
-        </div>
-        <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8">
-            <PostWidget />
-            <Categories />
-          </div>
-        </div>
+    <main className={styles["search"]}>
+      <div className={styles["searchbar"]}>
+        <form>
+          <input type="text" placeholder="Search..."></input>
+        </form>
       </div>
-    </div>
+      <div className={styles["flex-container"]}>
+        {posts.map((post, index) => (
+          <div className={styles["flex-item"]}>
+            <PostCardSmall key={index} post={post.node} />
+          </div>
+        ))}
+      </div>
+    </main>
+    // <div className="container mx-auto px-10 mb-8">
+    //   <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+    //     <div className="lg:col-span-8 col-span-1">
+    //       {posts.map((post, index) => (
+    //         <PostCard key={index} post={post.node} />
+    //       ))}
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
-Blog.layout = BlogLayout;
+Search.layout = BlogLayout;
 
 // Fetch data at build time
 export async function getStaticProps() {
@@ -60,5 +69,3 @@ export async function getStaticProps() {
     props: { posts },
   };
 }
-
-
