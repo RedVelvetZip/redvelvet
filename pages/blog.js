a6893ad59d8e356e86b25d860f6f9f014f4f9050
Bradley <bradley.t.sheehan@gmail.com>
2022-06-12 12:35:33 -0600

    blog home WIP

--- pages/blog.js
+++ pages/blog.js
@@ -1,22 +1,23 @@
 import { FeaturedPosts } from '../sections/index';
-import { PostCard, Categories, PostWidget } from '../components';
+import { PostCard, Categories, PostWidget, PostCardSmall } from '../components';
 import { getPosts } from '../services';
 import BlogLayout from '../components/Layout'
+import styles from '../styles/blog.module.scss'
 
 export default function Blog({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <FeaturedPosts />
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1">
+      <div className={styles["grid"]}>
+        <div className={styles["flex-container"]}>
           {posts.map((post, index) => (
-            <PostCard key={index} post={post.node} />
+            <PostCardSmall key={index} post={post.node} />
           ))}
         </div>
-        <div className="lg:col-span-4 col-span-1">
+        <div className={styles["recent-posts"]}>
           <div className="lg:sticky relative top-8">
             <PostWidget />
-            <Categories />
+            {/* <Categories /> */}
           </div>
         </div>
       </div>
