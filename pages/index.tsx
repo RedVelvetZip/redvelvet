536e01dde74b78f662bdde04dadf4fbfce405977
Bradley <bradley.t.sheehan@gmail.com>
2022-06-14 16:09:39 -0600

    homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,11 @@ import logo from "../assets/logo512.png"
 // import logoMp4 from "../assets/logo2.mp4"
 import DevLayout from '../components/dev/LayoutDev'
 import Link from "next/link";
+import emailLogo from '../assets/wallet-icons/email.svg'
+import githubLogo from "../assets/wallet-icons/github.svg";
+import twitterLogo from "../assets/wallet-icons/twitter.svg";
+import discordLogo from "../assets/wallet-icons/discord.svg";
+import linkedinLogo from "../assets/wallet-icons/linkedin.svg";
 function Dev() {
   return (
     <>
@@ -17,40 +22,67 @@ function Dev() {
         ></meta>
       </Head>
       <body className={styles["devbody"]}>
-      <main className={styles["dev"]}>
-        <div className={styles["blog"]}>
-          <Link href="/blog" passHref>
-            <span className={styles["blog-home"]}>
-              Blog
-              <svg
-                className={styles["icon"]}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M502.6 278.6l-128 128c-12.51 12.51-32.76 12.49-45.25 0c-12.5-12.5-12.5-32.75 0-45.25L402.8 288H32C14.31 288 0 273.7 0 255.1S14.31 224 32 224h370.8l-73.38-73.38c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l128 128C515.1 245.9 515.1 266.1 502.6 278.6z"
+        <main className={styles["dev"]}>
+          <div className={styles["icons-row"]}>
+            <div className={styles["icon-box"]}>
+              <a href="mailto:bradley.t.sheehan@gmail.com" target="_blank" rel="noreferrer" className={styles["icon-link"]}>
+                <Image alt="email" src={emailLogo}></Image>
+              </a>
+            </div>
+            <div className={styles["icon-box"]}>
+              <a href="https://github.com/RedVelvetZip" target="_blank" rel="noreferrer" className={styles["icon-link"]}>
+                <Image alt="GitHub" src={githubLogo}></Image>
+              </a>
+            </div>
+            <div className={styles["icon-box"]}>
+              <a href="https://twitter.com/RedVelvetZip" target="_blank" rel="noreferrer" className={styles["icon-link"]}>
+                <Image alt="Twitter" src={twitterLogo}></Image>
+              </a>
+            </div>
+            <div className={styles["icon-box"]}>
+              <a href="https://discordapp.com/users/redvelvet#4018" target="_blank" rel="noreferrer" className={styles["icon-link"]}>
+                <Image alt="Discord" src={discordLogo}></Image>
+              </a>
+            </div>
+            <div className={styles["icon-box"]}>
+              <a href="https://www.linkedin.com/in/bradleysheehan/" target="_blank" rel="noreferrer" className={styles["icon-link"]}>
+                <Image alt="LinkedIn" src={linkedinLogo} ></Image>
+              </a>
+            </div>
+          </div>
+          <div className={styles["blog"]}>
+            <Link href="/blog" passHref>
+              <span className={styles["blog-home"]}>
+                Blog
+                <svg
                   className={styles["icon"]}
-                />
-              </svg>
-            </span>
-          </Link>
-        </div>
-        <div className={styles["circle"]}>
-          <div className={styles["logo"]}>
-            <Image
-              src={logoGif}
-              alt="Red Velvet logo"
-              layout="responsive"
-              objectFit="contain"
-            >
-            </Image>
-            {/* <video autoPlay loop>
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M502.6 278.6l-128 128c-12.51 12.51-32.76 12.49-45.25 0c-12.5-12.5-12.5-32.75 0-45.25L402.8 288H32C14.31 288 0 273.7 0 255.1S14.31 224 32 224h370.8l-73.38-73.38c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l128 128C515.1 245.9 515.1 266.1 502.6 278.6z"
+                    className={styles["icon"]}
+                  />
+                </svg>
+              </span>
+            </Link>
+          </div>
+          <div className={styles["circle"]}>
+            <div className={styles["logo"]}>
+              <Image
+                src={logoGif}
+                alt="Red Velvet logo"
+                layout="responsive"
+                objectFit="contain"
+              >
+              </Image>
+              {/* <video autoPlay loop>
               <source src="/logo2.mp4" type="video/mp4" />
             </video> */}
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
       </body>
     </>
   );
