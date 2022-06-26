7c5b771ccc7b9096d29b134448a98c2cdc844ad7
Bradley <bradley.t.sheehan@gmail.com>
2022-06-26 11:19:28 -0600

    httpEquiv

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -28,7 +28,7 @@ const PostDetails = ({ post }) => {
       <Head>
         <title>{post.title}</title>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <meta name="description" content={post.excerpt} key="desc" />
         <meta property="og:title" content={post.title} />
