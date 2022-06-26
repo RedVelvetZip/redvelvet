807c1ba5e9400d7542d6f15a09da12e6e9d64425
Bradley <bradley.t.sheehan@gmail.com>
2022-06-26 11:39:39 -0600

    metadata

--- pages/blog.js
+++ pages/blog.js
@@ -1,27 +1,45 @@
-import { FeaturedPosts } from '../sections/index';
-import { PostCardSmall } from '../components';
-import { getPosts } from '../services';
-import BlogLayout from '../components/Layout'
-import styles from '../styles/blog.module.scss'
+import { FeaturedPosts } from "../sections/index";
+import { PostCardSmall } from "../components";
+import { getPosts } from "../services";
+import BlogLayout from "../components/Layout";
+import styles from "../styles/blog.module.scss";
 
 export default function Blog({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <FeaturedPosts />
-      <div className={styles["grid"]}>
-        <div className={styles["flex-container"]}>
-          {posts.map((post, index) => (
-            <PostCardSmall key={index} post={post.node} />
-          ))}
-        </div>
-        {/* <div className={styles["recent-posts"]}>
+    <>
+    <Head>
+          {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
+          <title>
+            Red Velvet Blog
+          </title>
+          {/* <meta
+          property="og:title"
+          content="Social Media Preview Working?"
+        /> */}
+          <meta name="twitter:card" content="summary" />
+          <meta
+            property="og:description"
+            content="Crypto, Coding, Finance & More"
+          />
+          <meta property="og:image" content={"url of image"} />
+        </Head>
+      <div className="container mx-auto px-10 mb-8">
+        <FeaturedPosts />
+        <div className={styles["grid"]}>
+          <div className={styles["flex-container"]}>
+            {posts.map((post, index) => (
+              <PostCardSmall key={index} post={post.node} />
+            ))}
+          </div>
+          {/* <div className={styles["recent-posts"]}>
           <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
           </div>
         </div> */}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 Blog.layout = BlogLayout;
@@ -33,4 +51,3 @@ export async function getStaticProps() {
     props: { posts },
   };
 }
-
