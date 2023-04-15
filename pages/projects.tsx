c57f58beea451065c5f08dcdf1b63a6e934c09cc
Bradley <bradley.t.sheehan@gmail.com>
2023-04-15 17:13:11 -0600

    projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -9,6 +9,7 @@ import discordLogo from "../assets/wallet-icons/discord.svg";
 import hack3rsLogo from "../assets/portfolio/Hack3rs.jpg";
 import infoLogo from "../assets/wallet-icons/info.svg";
 import moonLabLogo from "../assets/portfolio/MoonLab Logo4.png";
+import cryptoradoLogo from "../assets/portfolio/cryptoradoBanner_wide.png";
 import redvelvetLogo from "../assets/logo2.gif"
 import vqLogo from "../assets/portfolio/VQ Logo1.png";
 import DevLayout from '../components/dev/LayoutDev'
@@ -19,6 +20,86 @@ function Projects() {
             <h2 className={styles["header"]}>Current Projects</h2>
             <main className={styles["portfolio"]}>
                 <div className={styles["flex-container"]}>
+                    <div className={styles["flex-item"]}>
+                        <div className={styles["title"]}>
+                            <div className={styles["logo"]}>
+                                <a
+                                    className={styles["logo-case"]}
+                                    href="http://cryptorado.org/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <Image
+                                        objectFit='contain'
+                                        src={cryptoradoLogo}
+                                        alt="cryptorado"
+                                    ></Image>
+                                </a>
+                            </div>
+                            <div className={styles["links"]}>
+                                <div className={styles["links-item"]}>
+                                    <div>
+                                        <a
+                                            href="http://cryptorado.org/"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <Image
+                                                object-fit="contain"
+                                                src={linkLogo}
+                                                alt="Cryptorado website"
+                                            ></Image>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className={styles["links-item"]}>
+                                    <div>
+                                        <a
+                                            href="https://twitter.com/CryptoradoCom"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <Image
+                                                object-fit="contain"
+                                                src={twitterLogo}
+                                                alt="MoonLab Twitter"
+                                            ></Image>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className={styles["links-item"]}>
+                                    <div>
+                                        <a
+                                            href="https://discord.com/invite/gWcej9rUxN"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <Image
+                                                object-fit="contain"
+                                                src={discordLogo}
+                                                alt="Cryptorado Discord"
+                                            ></Image>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles["project"]}>
+                            {/* <p className={styles["name"]}>
+                            MoonLab
+                            </p> */}
+                            <p className={styles["role"]}>
+                                Event Host, Cryptorado
+                            </p>
+                        </div>
+                        <p className={styles["description"]}>
+                            Cryptorado is a community in Colorado for all things crypto. Coworking, lectures/workshops, hangouts, and adventures. I speak at and help organize some of these events. Lectures, panels, and more!
+
+                        </p>
+                        <p className={styles["stack"]}>
+                            Tools: Meetup, Discord
+                        </p>
+                    </div>
                     <div className={styles["flex-item"]}>
                         <div className={styles["title"]}>
                             <div className={styles["logo"]}>
@@ -104,148 +185,6 @@ function Projects() {
                             <div className={styles["logo"]}>
                                 <a
                                     className={styles["logo-case"]}
-                                    href="https://hack3rs.hackathon.com/"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <Image
-                                        // layout='fill'
-                                        objectFit='contain'
-                                        src={hack3rsLogo}
-                                        alt="Hack3rsClub"
-                                    ></Image>
-                                </a>
-                            </div>
-                            <div className={styles["links"]}>
-                                <div className={styles["links-item"]}>
-                                    <div >
-                                        <a
-                                            href="https://hack3rs.hackathon.com/"
-                                            target="_blank"
-                                            rel="noreferrer"
-                                        >
-                                            <Image
-                                                object-fit="contain"
-                                                src={linkLogo}
-                                                alt="Hack3rsClub Website"
-                                            ></Image>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className={styles["links-item"]}>
-                                    <div >
-                                        <a
-                                            href="https://discord.com/invite/XAhxSgaM3E"
-                                            target="_blank"
-                                            rel="noreferrer"
-                                        >
-                                            <Image
-                                                object-fit="contain"
-                                                src={discordLogo}
-                                                alt="Hack3rsClub Discord"
-                                            ></Image>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={styles["project"]}>
-                            <p className={styles["name"]}>
-                                Educator, Hack3rsClub
-                            </p>
-                        </div>
-                        <p className={styles["description"]}>
-                            Hack3rsClub is a community of developers hosted by BeMyApp and Hackathon.com. I <strong>teach Web2 developers the basics of Web3 development</strong> through webinars, code-alongs, and workshops.
-                        </p>
-                        <p className={styles["stack"]}>
-                            Tools: Discord, Solidity, VS Code, React, Canva
-                        </p>
-                    </div>
-                    <div className={styles["flex-item"]}>
-                        <div className={styles["title"]}>
-                            <div className={styles["logo"]}>
-                                <a
-                                    className={styles["logo-case"]}
-                                    href="https://moonlab.space"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <Image
-                                        objectFit='contain'
-                                        src={moonLabLogo}
-                                        alt="MoonLab"
-                                    ></Image>
-                                </a>
-                            </div>
-                            <div className={styles["links"]}>
-                                <div className={styles["links-item"]}>
-                                    <div>
-                                        <a
-                                            href="https://www.youtube.com/watch?v=qImzfh39GXs"
-                                            target="_blank"
-                                            rel="noreferrer"
-                                        >
-                                            <Image
-                                                object-fit="contain"
-                                                src={linkLogo}
-                                                alt="MoonLab Youtube"
-                                            ></Image>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className={styles["links-item"]}>
-                                    <div>
-                                        <a
-                                            href="https://twitter.com/Moonlab_HQ"
-                                            target="_blank"
-                                            rel="noreferrer"
-                                        >
-                                            <Image
-                                                object-fit="contain"
-                                                src={twitterLogo}
-                                                alt="MoonLab Twitter"
-                                            ></Image>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className={styles["links-item"]}>
-                                    <div>
-                                        <a
-                                            href="https://discord.gg/2gwcaPK4xK"
-                                            target="_blank"
-                                            rel="noreferrer"
-                                        >
-                                            <Image
-                                                object-fit="contain"
-                                                src={discordLogo}
-                                                alt="MoonLab Discord"
-                                            ></Image>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={styles["project"]}>
-                            {/* <p className={styles["name"]}>
-                            MoonLab
-                            </p> */}
-                            <p className={styles["role"]}>
-                                Speaker, MoonLab Meets & Cryptorado
-                            </p>
-                        </div>
-                        <p className={styles["description"]}>
-                            MoonLab Meets & Cryptorado host educational events in Denver. I speak at and help organize these events. Lectures, panels, and more!
-
-                        </p>
-                        <p className={styles["stack"]}>
-                            Tools: Meetup, Eventbrite, Streamyard, Discord
-                        </p>
-                    </div>
-                    <div className={styles["flex-item"]}>
-                        <div className={styles["title"]}>
-                            <div className={styles["logo"]}>
-                                <a
-                                    className={styles["logo-case"]}
                                     href="https://redvelvet.tech/blog/"
                                     target="_blank"
                                     rel="noreferrer"
@@ -301,6 +240,68 @@ function Projects() {
                         </p>
                         <p className={styles["stack"]}>
                             Tools: NextJS, GraphCMS, Canva
+                        </p>
+                    </div>
+                    <div className={styles["flex-item"]}>
+                        <div className={styles["title"]}>
+                            <div className={styles["logo"]}>
+                                <a
+                                    className={styles["logo-case"]}
+                                    href="https://hack3rs.hackathon.com/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <Image
+                                        // layout='fill'
+                                        objectFit='contain'
+                                        src={hack3rsLogo}
+                                        alt="Hack3rsClub"
+                                    ></Image>
+                                </a>
+                            </div>
+                            <div className={styles["links"]}>
+                                <div className={styles["links-item"]}>
+                                    <div >
+                                        <a
+                                            href="https://hack3rs.hackathon.com/"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <Image
+                                                object-fit="contain"
+                                                src={linkLogo}
+                                                alt="Hack3rsClub Website"
+                                            ></Image>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className={styles["links-item"]}>
+                                    <div >
+                                        <a
+                                            href="https://discord.com/invite/XAhxSgaM3E"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <Image
+                                                object-fit="contain"
+                                                src={discordLogo}
+                                                alt="Hack3rsClub Discord"
+                                            ></Image>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles["project"]}>
+                            <p className={styles["name"]}>
+                                Educator, Hack3rsClub
+                            </p>
+                        </div>
+                        <p className={styles["description"]}>
+                            Hack3rsClub is a community of developers hosted by BeMyApp and Hackathon.com. I <strong>teach Web2 developers the basics of Web3 development</strong> through webinars, code-alongs, and workshops.
+                        </p>
+                        <p className={styles["stack"]}>
+                            Tools: Discord, Solidity, VS Code, React, Canva
                         </p>
                     </div>
                 </div>
