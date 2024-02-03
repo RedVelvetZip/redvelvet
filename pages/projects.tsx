4972d9577e0de67ec0ea140d58732833b04b0b8d
Red <bradley.t.sheehan@gmail.com>
2024-02-03 13:55:55 -0700

    update current projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -11,6 +11,8 @@ import hack3rsLogo from "../assets/portfolio/Hack3rs.jpg";
 // import infoLogo from "../assets/wallet-icons/info.svg";
 import moonLabLogo from "../assets/portfolio/MoonLab Logo4.png";
 import cryptoradoLogo from "../assets/portfolio/cryptoradoBanner_wide.png";
+import messariLogo from "../assets/portfolio/messari.png";
+import l2bitLogo from "../assets/portfolio/l2bit.png";
 import redvelvetLogo from "../assets/logo2.gif"
 // import vqLogo from "../assets/portfolio/VQ Logo1.png";
 import DevLayout from '../components/dev/LayoutDev'
@@ -21,6 +23,125 @@ function Projects() {
             <h2 className={styles["header"]}>Current Projects</h2>
             <main className={styles["portfolio"]}>
                 <div className={styles["flex-container"]}>
+                <div className={styles["flex-item"]}>
+                        <div className={styles["title"]}>
+                            <div className={styles["logo"]}>
+                                <a
+                                    className={styles["logo-case"]}
+                                    href="https://messari.io/research/red-sheehan"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <Image
+                                        objectFit='contain'
+                                        src={messariLogo}
+                                        alt="messari"
+                                    ></Image>
+                                </a>
+                            </div>
+                            <div className={styles["links"]}>
+                                <div className={styles["links-item"]}>
+                                    <div>
+                                        <a
+                                            href="http://cryptorado.org/"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <Image
+                                                object-fit="contain"
+                                                src={linkLogo}
+                                                alt="Cryptorado website"
+                                            ></Image>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className={styles["links-item"]}>
+                                    <div>
+                                        <a
+                                            href="https://twitter.com/messaricrypto"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <Image
+                                                object-fit="contain"
+                                                src={twitterLogo}
+                                                alt="Twitter"
+                                            ></Image>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles["project"]}>
+                            <p className={styles["role"]}>
+                                Research Analyst, Messari
+                            </p>
+                        </div>
+                        <p className={styles["description"]}>
+                            Messari is the Bloomberg of crypto. I'm a research analyst covering base layer protocols (L1s and L2s) and privacy protocols.
+                        </p>
+                    </div>
+                    <div className={styles["flex-item"]}>
+                        <div className={styles["title"]}>
+                            <div className={styles["logo"]}>
+                                <a
+                                    className={styles["logo-case"]}
+                                    href="http://bitcoinrollups.io/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <Image
+                                        objectFit='contain'
+                                        src={l2bitLogo}
+                                        alt="l2bit"
+                                    ></Image>
+                                </a>
+                            </div>
+                            <div className={styles["links"]}>
+                                <div className={styles["links-item"]}>
+                                    <div>
+                                        <a
+                                            href="https://www.bitcoinrollups.io/"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <Image
+                                                object-fit="contain"
+                                                src={linkLogo}
+                                                alt="Bitcoin Rollups website"
+                                            ></Image>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className={styles["links-item"]}>
+                                    <div>
+                                        <a
+                                            href="https://twitter.com/BitcoinRollups"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <Image
+                                                object-fit="contain"
+                                                src={twitterLogo}
+                                                alt="Twitter"
+                                            ></Image>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles["project"]}>
+                            {/* <p className={styles["name"]}>
+                            MoonLab
+                            </p> */}
+                            <p className={styles["role"]}>
+                                Builder/Educator, Bitcoin Rollups
+                            </p>
+                        </div>
+                        <p className={styles["description"]}>
+                            Documenting Bitcoin L2s and rollups. I contribute frontend development and research to this project.
+                        </p>
+                    </div>
                     <div className={styles["flex-item"]}>
                         <div className={styles["title"]}>
                             <div className={styles["logo"]}>
@@ -112,9 +233,6 @@ function Projects() {
                             Cryptorado is a community in Colorado for all things crypto. Coworking, lectures/workshops, hangouts, and adventures. I speak at and help organize some of these events. Lectures, panels, and more!
 
                         </p>
-                        {/* <p className={styles["stack"]}>
-                            Tools: Meetup, Discord
-                        </p> */}
                     </div>
                     {/* <div className={styles["flex-item"]}>
                         <div className={styles["title"]}>
