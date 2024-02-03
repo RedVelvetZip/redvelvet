af57f954a7f859841cf0fdc20e9cb072f0654f8f
Red <bradley.t.sheehan@gmail.com>
2024-02-03 14:02:42 -0700

    homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import githubLogo from "../assets/wallet-icons/github.svg";
 import twitterLogo from "../assets/wallet-icons/twitter.svg";
 import discordLogo from "../assets/wallet-icons/discord.svg";
 import linkedinLogo from "../assets/wallet-icons/linkedin.svg";
+import warpLogo from "../assets/wallet-icons/warpLogo.jpg";
 import { InlineWidget } from "react-calendly";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -35,8 +36,13 @@ function Dev() {
         <main className={styles["dev"]}>
           <div className={styles["icons-row"]}>
             <div className={styles["icon-box"]}>
-              <a href="mailto:bradley.t.sheehan@gmail.com" target="_blank" rel="noreferrer" className={styles["icon-link"]}>
-                <Image alt="email" src={emailLogo}></Image>
+              <a href="https://twitter.com/RedVelvetZip" target="_blank" rel="noreferrer" className={styles["icon-link"]}>
+                <Image alt="Twitter" src={twitterLogo}></Image>
+              </a>
+            </div>
+            <div className={styles["icon-box"]}>
+              <a href="https://warpcast.com/redvelvet" target="_blank" rel="noreferrer" className={styles["icon-link"]}>
+                <Image alt="farcaster" src={warpLogo}></Image>
               </a>
             </div>
             <div className={styles["icon-box"]}>
@@ -45,15 +51,10 @@ function Dev() {
               </a>
             </div>
             <div className={styles["icon-box"]}>
-              <a href="https://twitter.com/RedVelvetZip" target="_blank" rel="noreferrer" className={styles["icon-link"]}>
-                <Image alt="Twitter" src={twitterLogo}></Image>
+              <a href="mailto:bradley.t.sheehan@gmail.com" target="_blank" rel="noreferrer" className={styles["icon-link"]}>
+                <Image alt="email" src={emailLogo}></Image>
               </a>
             </div>
-            {/* <div className={styles["icon-box"]}>
-              <a href="https://discordapp.com/users/redvelvet#4018" target="_blank" rel="noreferrer">
-                <Image alt="Discord" src={discordLogo}></Image>
-              </a>
-            </div> */}
             <div className={styles["icon-box"]}>
               <a href="https://www.linkedin.com/in/bradleysheehan/" target="_blank" rel="noreferrer" className={styles["icon-link"]}>
                 <Image alt="LinkedIn" src={linkedinLogo} ></Image>
@@ -61,9 +62,27 @@ function Dev() {
             </div>
           </div>
           <div className={styles["blog"]}>
-            <Link href="/blog" passHref>
+            <Link href="https://messari.io/research/red-sheehan" passHref>
               <span className={styles["blog-home"]}>
-                Blog
+                Messari Research
+                <svg
+                  className={styles["icon"]}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M502.6 278.6l-128 128c-12.51 12.51-32.76 12.49-45.25 0c-12.5-12.5-12.5-32.75 0-45.25L402.8 288H32C14.31 288 0 273.7 0 255.1S14.31 224 32 224h370.8l-73.38-73.38c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l128 128C515.1 245.9 515.1 266.1 502.6 278.6z"
+                    className={styles["icon"]}
+                  />
+                </svg>
+              </span>
+            </Link>
+          </div>
+          <div className={styles["blog"]}>
+            <Link href="https://redvelvetzip.substack.com/" passHref>
+              <span className={styles["blog-home"]}>
+                Personal Tech Blog
                 <svg
                   className={styles["icon"]}
                   xmlns="http://www.w3.org/2000/svg"
