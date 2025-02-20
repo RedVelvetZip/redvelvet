c133a138b88bdf26ce5d03714b43c468a02fac1e
Red <bradley.t.sheehan@gmail.com>
2025-02-19 17:47:23 -0700

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AppProps } from "next/app";
 import "../styles/globals.scss";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
